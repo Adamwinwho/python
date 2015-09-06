1d8ec73518f22935630f331ace7c54ba0e66c9ab
Adamwinwho <hs15952016403@gmail.com>
2015-09-06 19:09:31 +0800

    python function learning
    about slice in python

---
+++ base/function.py
@@ -0,0 +1,11 @@
+#slice
+L=['hanyue','mengyun','zhichi','tianya']
+#copy a list from L,index form 0 to 3 but not include 3
+L1 = L[0:3]
+print(L1)
+#copy a same list
+L2= L[:]
+print(L2)
+#cpoy a list from L every 2 element
+L3 = L[::5]
+print(L3)
